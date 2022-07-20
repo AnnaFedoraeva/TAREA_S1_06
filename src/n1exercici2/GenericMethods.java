34f510e22c9b454b3454f51a8bf2d98e7d139229
AnnaFedoraeva <anafedoraeva@gmail.com>
2022-07-20 10:04:34 +0200

    modified

--- src/n1exercici2/GenericMethods.java
+++ src/n1exercici2/GenericMethods.java
@@ -5,15 +5,14 @@ public class GenericMethods<T, V, U> {
 	T ob1;
 
 	V ob2;
-	
+
 	U ob3;
 
 	public GenericMethods(T ob1, V ob2, U ob3) {
-		super();
 		this.ob1 = ob1;
 		this.ob2 = ob2;
 		this.ob3 = ob3;
-		
+
 	}
 
 	public T getOb1() {
@@ -23,7 +22,7 @@ public class GenericMethods<T, V, U> {
 	public V getOb2() {
 		return ob2;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "GenericMethods [ob1=" + ob1 + ", ob2=" + ob2 + ", ob3=" + ob3 + "]";
@@ -33,18 +32,10 @@ public class GenericMethods<T, V, U> {
 		return ob3;
 	}
 
-
-	void showTypes()
-	       {
-	           System.out.println("Type of T is : " + ob1.getClass().getName());
-	           System.out.println("Type of V is : " + ob2.getClass().getName());
-	           System.out.println("Type of U is : " + ob3.getClass().getName());
-	       }
-	
-	void print () {
-		System.out.println(getOb1().toString());
-		System.out.println(getOb2().toString());
-		System.out.println(getOb3().toString());
+	public static <T, V, U> void generic (T ob1, V ob2, U ob3) {
+		System.out.println(ob1);
+		System.out.println(ob2);
+		System.out.println(ob3);
 	}
 
 }
--- src/n1exercici2/Principal.java
+++ src/n1exercici2/Principal.java
@@ -4,22 +4,27 @@ public class Principal {
 
 	public static void main(String[] args) {
 
-		GenericMethods<String, String, Integer> obj = new GenericMethods<String, String, Integer>("Lola", "Gomez", 15);
-		obj.showTypes();
-		obj.print();
+		// llamamos a un método genérico de la clase GenericMethods que acepte tres
+		// argumentos de tipo genérico
 
-		GenericMethods<Persona, String, Integer> genObj = generic();
-		genObj.showTypes();
-		genObj.print();
+		GenericMethods.generic("Lisa", "Lopez", 23);
+		GenericMethods.generic("Lisa", 1.60, 23);
+		GenericMethods.generic(23, "Lisa", "Lopez");
+		GenericMethods.generic(new Persona("Bea", "Lopez", 21), "Camarera", 1500);
+
+		// llamamos metodo genericMain con distintos tipos de parámetros
+
+		genericMain(new Persona("Lisa", "Lopez", 23), "Camarera", 1450);
+		genericMain("Lisa", "Lopez", 23);
+		genericMain("Lisa", 1.60, 23);
+		genericMain(23, "Lisa", "Lopez");
 
 	}
 
-	public static GenericMethods<Persona, String, Integer> generic() {
-		GenericMethods<Persona, String, Integer> obj1 = new GenericMethods<Persona, String, Integer>(
-				new Persona("Bea", "Lopez", 21), "Camarera", 1500);
-		obj1.print();
-		return obj1;
-
+	public static <T, V, U> void genericMain(T ob1, V ob2, U ob3) {
+		System.out.println(ob1);
+		System.out.println(ob2);
+		System.out.println(ob3);
 	}
 
 }
